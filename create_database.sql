3e8a293171f86833b2d7247951521632e7aea717
Jochimin <54875738+jochimin88@users.noreply.github.com>
2020-11-03 02:35:58 +0100

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -1,1 +1,32 @@
 
+
+ /* create database ejemplo;*/
+
+CREATE DATABASE ejemplo
+
+ /* use database ejemplo*/
+
+USE ejemplo
+
+/* create table Usuarios inside of database ejemplo
+  with Nombre, Usuario, Password,and Tipo_usuario
+*/
+
+create table Usuarios(
+Id_usuario INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+Nombre VARCHAR(50) NULL,
+Usuario VARCHAR(50) NULL,
+Password VARCHAR(50) NULL,
+Tipo_usuario VARCHAR(50) NULL,
+)
+
+
+/* Insert this values in the tables*/
+
+insert into Usuarios values ('Jochimin Contreras', 'jochimin', '123456789', 'Admin'),
+                                ('Fidel Contreras', 'fidel', '123456789', 'Usuario');
+
+
+/*Show all the tables from Usuarios table */
+
+select * from Usuarios
